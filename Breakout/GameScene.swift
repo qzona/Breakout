b45f6faea06e1be1dab178c88b6d7f1d49e74dd7
Quinn Zona <qzona@bsd220.org>
2022-03-08 10:48:18 -0600

    set up display labels

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -7,6 +7,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
+    var playLabel = SKLabelNode()
+       var livesLabel = SKLabelNode()
+       var scoreLabel = SKLabelNode()
+       var playingGame = false
+       var score = 0
+       var lives = 3
 
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
@@ -15,17 +21,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createBackground()
         resetGame()
         makeLoseZone()
-        kickBall()
+        makeLabels()
     }
     func resetGame() {
             // this stuff happens before each game starts
             makeBall()
         makePaddle()
         makeBrick()
+        updateLabels()
         }
     func kickBall() {
            ball.physicsBody?.isDynamic = true
            ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
+       }
+    func updateLabels() {
+           scoreLabel.text = "Score: \(score)"
+           livesLabel.text = "Lives: \(lives)"
        }
 
     func createBackground() {
@@ -95,11 +106,44 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             loseZone.physicsBody?.isDynamic = false
             addChild(loseZone)
         }
+    func makeLabels() {
+           playLabel.fontSize = 24
+           playLabel.text = "Tap to start"
+           playLabel.fontName = "Arial"
+           playLabel.position = CGPoint(x: frame.midX, y: frame.midY - 50)
+           playLabel.name = "playLabel"
+           addChild(playLabel)
+           
+           livesLabel.fontSize = 18
+           livesLabel.fontColor = .black
+           livesLabel.position = CGPoint(x: frame.minX + 50, y: frame.minY + 18)
+           addChild(livesLabel)
+           
+           scoreLabel.fontSize = 18
+           scoreLabel.fontColor = .black
+           scoreLabel.fontName = "Arial"
+           scoreLabel.position = CGPoint(x: frame.maxX - 50, y: frame.minY + 18)
+           addChild(scoreLabel)
+       }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-                    let location = touch.location(in: self)
-                    paddle.position.x = location.x
-                }
+           for touch in touches {
+               let location = touch.location(in: self)
+               if playingGame {
+                   paddle.position.x = location.x
+               }
+               else {
+                   for node in nodes(at: location) {
+                       if node.name == "playLabel" {
+                           playingGame = true
+                           node.alpha = 0
+                           score = 0
+                           lives = 3
+                           updateLabels()
+                           kickBall()
+                       }
+                   }
+               }
+           }
        }
        
        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
